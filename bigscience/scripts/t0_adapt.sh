ffd603481fb2877c2977e3c4c6581d8b66dfae22
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2021-12-25 15:25:28 +0100

    Allow model to query HF hub

--- bigscience/scripts/t0_adapt.sh
+++ bigscience/scripts/t0_adapt.sh
@@ -37,7 +37,7 @@ GIN_FILE=bigscience/gins/$GIN_FILE
 echo "Running the following config: $GIN_FILE" 2>&1 | tee $LOGS_PATH/pretrain_$EXPERIMENT_NAME.txt
 
 # We use offline as loading seqio can be quite long.
-HF_DATASETS_OFFLINE=1 HF_DATASETS_OFFLINE=1 python3 ${T5X_DIR}/t5x/train.py \
+python3 ${T5X_DIR}/t5x/train.py \
   --gin_file="$GIN_FILE" \
   --gin.INITIAL_CHECKPOINT_PATH="'${CHECKPOINT_DIR}'" \
   --gin.MODEL_DIR="'${MODEL_DIR}'" \
