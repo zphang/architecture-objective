b780ebb2dbd2260fa33dcd3e4527ff5acd9ccabe
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2021-12-29 16:47:35 +0100

    Plot agregated values + random_baseline

--- bigscience/scripts/plot_t5x_results_vs_t0.py
+++ bigscience/scripts/plot_t5x_results_vs_t0.py
@@ -7,6 +7,9 @@ from argparse import ArgumentParser
 import matplotlib.pyplot as plt
 from pathlib import Path
 
+import numpy as np
+
+
 def get_args():
     parser = ArgumentParser()
     parser.add_argument('--all', action="store_true", help="Plot all results in a single plot")
@@ -56,25 +59,26 @@ def get_experiment_name(filename: str):
     return name
 
 TASKS = {
-    'super_glue_copa': 'COPA',
-    'anli_r1': 'ANLI R1',
-    'anli_r2': 'ANLI R2',
-    'anli_r3': 'ANLI R3',
-    'super_glue_cb': 'CB',
-    'super_glue_rte': 'RTE',
-    'super_glue_wsc.fixed': 'WSC',
-    'winogrande_winogrande_xl': 'Winogrande',
-    'super_glue_wic': 'WiC',
-    'hellaswag': 'HellaSwag',
-    'story_cloze_2016': 'StoryCloze',
+    'super_glue_copa': ('COPA', 0.5),
+    'anli_r1': ('ANLI R1', 1/3),
+    'anli_r2': ('ANLI R2', 1/3),
+    'anli_r3': ('ANLI R3', 1/3),
+    'super_glue_cb': ('CB', 1/3),
+    'super_glue_rte': ('RTE', 0.5),
+    'super_glue_wsc.fixed': ('WSC', 0.5),
+    'winogrande_winogrande_xl': ('Winogrande', 0.5),
+    'super_glue_wic': ('WiC', 0.5),
+    'hellaswag': ('HellaSwag', 0.25),
+    'story_cloze_2016': ('StoryCloze', 0.5),
 }
 def plot(t5x_data, t0_data):
     t5x_data, t5x_experiments = t5x_data
-
+    assert len(TASKS) == 11
     fig, axs = plt.subplots(2, 6, figsize=(20, 8))
     axs = axs.flatten()
 
-    for n, (task, name) in enumerate(TASKS.items()):
+    task_median_score = {}
+    for n, (task, (name, random_baseline)) in enumerate(TASKS.items()):
         t5lm_scores = [float(r["score"]) for r in t0_data
                        if r["runs"] == "xxl-lm-d4-091621"
                        and r["dataset_name"] == task
@@ -87,11 +91,39 @@ def plot(t5x_data, t0_data):
                      and r["score"]]
         t5x_scores = [[s["accuracy"] for k, s in t5x_data[name].items() if task.replace("anli_", "") in k]
                       for name in t5x_experiments]
+
+        # Plot
+        axs[n].axhline(100 * random_baseline, 0, len([t5lm_scores, t0_scores, *t5x_scores]))
         for i, scores in enumerate([t5lm_scores, t0_scores, *t5x_scores]):
             axs[n].scatter([i] * len(scores), scores, s=50, alpha=0.4)
         axs[n].set_title(name)
-    axs[10].legend(["T5+LM", "T0", *[get_experiment_name(exp) for exp in t5x_experiments]], bbox_to_anchor=(1, 1), loc="upper left")
-    axs[11].set_visible(False)
+
+        # Gather median values
+        task_median_score[task] = [random_baseline] + [np.median(scores) for scores in [t5lm_scores, t0_scores, *t5x_scores]]
+
+    last_ax_id = len(TASKS) - 1
+    axs[last_ax_id].legend(["T5+LM", "T0", *[get_experiment_name(exp) for exp in t5x_experiments]],
+                           bbox_to_anchor=(1, 1), loc="upper left")
+    for ax in axs[last_ax_id + 1:]:
+        ax.set_visible(False)
+
+    # ====== Plot agregated values =======
+    fig, axs = plt.subplots(1, 2, figsize=(20, 8))
+    axs = axs.flatten()
+    last_ax_id = 0
+
+    # Plot average task median score
+    average_task_media_score = np.mean(list(task_median_score.values()), axis=0)
+    axs[last_ax_id].axhline(100 * average_task_media_score[0], 0, len(average_task_media_score) -1)
+    for i, score in enumerate(average_task_media_score[1:]):
+        axs[last_ax_id].scatter(i, score, s=50)
+    axs[last_ax_id].set_title("Average of task median scores")
+
+    axs[last_ax_id].legend(["Random", "T5+LM", "T0", *[get_experiment_name(exp) for exp in t5x_experiments]],
+                           bbox_to_anchor=(1, 1), loc="upper left")
+    for ax in axs[last_ax_id + 1:]:
+        ax.set_visible(False)
+
 
 def main():
     args = get_args()
