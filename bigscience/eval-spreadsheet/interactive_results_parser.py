9c8a41c004a8cb05c37c9bdaf0d25b75d8361462
Julien Launay <julien@lighton.ai>
2022-01-14 17:21:34 +0100

    🆕 Add prompt source parser for eval spreadsheet

--- bigscience/eval-spreadsheet/interactive_results_parser.py
+++ bigscience/eval-spreadsheet/interactive_results_parser.py
@@ -3,6 +3,7 @@ import json
 import time
 
 from parse_eai_results import process_eai_results
+from parse_promptsource import process_ps_results
 
 if __name__ == "__main__":
     previous_clipboard = clipboard.paste()
@@ -11,7 +12,11 @@ if __name__ == "__main__":
         if new_clipboard != previous_clipboard:
             previous_clipboard = new_clipboard
             try:
-                results = process_eai_results(json.loads(new_clipboard))
+                results_raw = json.loads(new_clipboard)
+                if "results" in results_raw:
+                    results = process_eai_results(results_raw)
+                else:
+                    results = process_ps_results(results_raw)
                 previous_clipboard = results
             except Exception as e:
                 print("🔴 Clipboard content is not a valid EAI results JSON!")
---
+++ bigscience/eval-spreadsheet/parse_promptsource.py
@@ -0,0 +1,107 @@
+import argparse
+import clipboard
+import numpy as np
+
+from utils import extract_json
+
+
+def process_ps_tasks(ps_results: dict, to_clipboard: bool = True) -> str:
+    spreadsheet_content, previous_tasks = "", set()
+    for task_prompt in ps_results:
+        task, prompt = process_task_prompt(task_prompt)
+        print(task, prompt)
+        if task not in previous_tasks:
+            spreadsheet_content += f"{task}\tacc_avg\n\tacc_best\n"
+        previous_tasks.add(task)
+
+    if to_clipboard:
+        clipboard.copy(spreadsheet_content)
+        print("🟢 Results added to clipboard, ready to paste in spreadsheet!")
+
+    return spreadsheet_content
+
+
+def process_ps_results(ps_resuls:dict, to_clipboard: bool = True) -> str:
+    spreadsheet_content = ""
+    formatted_results = {}
+    for task_prompt in ps_resuls:
+        task, prompt = process_task_prompt(task_prompt)
+
+        if task not in formatted_results:
+            formatted_results[task] = {}
+
+        formatted_results[task][prompt] = ps_resuls[task_prompt]["accuracy"]
+
+    for task in formatted_results:
+        results = list(formatted_results[task].values())
+        acc_avg, acc_std, acc_best = np.mean(results) / 100, np.std(results) / 100, max(results) / 100
+        spreadsheet_content += f"{acc_avg}\t{acc_std}\n{acc_best}\n"
+
+    if to_clipboard:
+        clipboard.copy(spreadsheet_content)
+        print("🟢 Results added to clipboard, ready to paste in spreadsheet!")
+
+    return spreadsheet_content
+
+
+def process_task_prompt(task_prompt: str) -> tuple[str, str]:
+    task_prompt = task_prompt[:-11]  # Remove 'score_eval' string at the end
+
+    task, prompt = None, None
+    if "anli" in task_prompt:
+        task = "anli" + task_prompt[-3:]
+        prompt = task_prompt[5:-3]
+    elif "hellaswag" in task_prompt:
+        task = "hellaswag"
+        prompt = task_prompt[10:]
+    elif "story_cloze" in task_prompt:
+        task = "story_cloze"
+        prompt = task_prompt[17:]
+    elif "super_glue" in task_prompt:
+        if "cb" in task_prompt:
+            task = "cb"
+            prompt = task_prompt[14:]
+        elif "copa" in task_prompt:
+            task = "copa"
+            prompt = task_prompt[16:]
+        elif "rte" in task_prompt:
+            task = "rte"
+            prompt = task_prompt[15:]
+        elif "wic" in task_prompt:
+            task = "wic"
+            prompt = task_prompt[15:]
+        elif "wsc" in task_prompt:
+            task = "wsc"
+            prompt = task_prompt[15:]
+    elif "winogrande" in task_prompt:
+        task = "winogrande"
+        prompt = task_prompt[25:]
+
+    if task is None or prompt is None:
+        raise ValueError(f"Failed to parse task/prompt: {task_prompt}")
+
+    return task, prompt
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Get formatted promptsource tasks & results for c/p in spreadsheet"
+    )
+    parser.add_argument(
+        "-i",
+        "--ps-results-file",
+        type=str,
+        help="Path to a promptsource .json result file",
+    )
+    parser.add_argument(
+        "-t",
+        "--get-tasks",
+        action="store_true",
+        help="Get tasks headers instead of results",
+    )
+    args = parser.parse_args()
+
+    if args.get_tasks:
+        process_ps_tasks(extract_json(args.ps_results_file))
+    else:
+        process_ps_results(extract_json(args.ps_results_file))
